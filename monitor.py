8c98e846137df7bcfbc7061473e50516ec0515f2
Fredrik Ahlberg <fredrik@z80.se>
2013-01-09 19:39:21 +0100

    wip monitor

--- monitor.py
+++ monitor.py
@@ -22,6 +22,7 @@ class MonitorPacket:
     MON_READREG     = 9
     MON_WRITEREG    = 10
     MON_CALL        = 11
+    MON_PLUGIN      = 12
 
     def __init__(self, cmd = 0, payload = ''):
         (self.cmd, self.payload) = (cmd, payload)
@@ -65,18 +66,22 @@ class Monitor(object):
         self.firmware = pkt.payload
 
     def read_mem(self, addr, length):
-        pkt = MonitorPacket()
-        pkt.cmd = pkt.MON_READMEM
-        pkt.payload = struct.pack('<II', addr, length)
-        pkt.to_socket(self.s)
+        data = ''
+        while len(data) < length:
+            to_read = min(length, 64)
+            pkt = MonitorPacket()
+            pkt.cmd = pkt.MON_READMEM
+            pkt.payload = struct.pack('<II', addr + len(data), to_read)
+            pkt.to_socket(self.s)
 
-        pkt = MonitorPacket()
-        pkt.from_socket(self.s)
-        if pkt.cmd != pkt.MON_READMEM:
-            print 'Unexpected packet ', pkt
-            sys.exit(1)
+            pkt = MonitorPacket()
+            pkt.from_socket(self.s)
+            if pkt.cmd != pkt.MON_READMEM:
+                print 'Unexpected packet ', pkt
+                sys.exit(1)
+            data = data + pkt.payload[8:]
 
-        return map(ord, pkt.payload[8:])
+        return map(ord, data)
 
     def write_mem(self, addr, data):
         pkt = MonitorPacket()
@@ -165,6 +170,20 @@ class Monitor(object):
             sys.exit(1)
         (addr, r0) = struct.unpack('<II', pkt.payload)
         return r0
+
+    def plugin(self, state):
+        pkt = MonitorPacket()
+        pkt.cmd = pkt.MON_PLUGIN
+        pkt.payload = struct.pack('<I', state)
+        pkt.to_socket(self.s)
+
+        pkt = MonitorPacket()
+        pkt.from_socket(self.s)
+        if pkt.cmd != pkt.MON_PLUGIN:
+            print 'Unexpected packet ', pkt
+            sys.exit(1)
+        (state, ) = struct.unpack('<I', pkt.payload)
+        return state
 
 commands = {}
 
@@ -256,20 +275,21 @@ class InteractiveMonitor(Monitor):
         """Reboots the module. Monitor will be disconnected."""
         Monitor.reboot(self)
         print '  Rebooting.'
-        time.sleep(5)
+        sys.exit(0)
+        #time.sleep(5)
 
-        print '*** Reconnecting to %s-%d...' % (self.addr, self.port),
-        sys.stdout.flush()
+        #print '*** Reconnecting to %s-%d...' % (self.addr, self.port),
+        #sys.stdout.flush()
 
-        try:
-            self.reconnect()
-        except BluetoothError as e:
-            print 'failed: %s' % e
-            sys.exit(1)
+        #try:
+        #    self.reconnect()
+        #except BluetoothError as e:
+        #    print 'failed: %s' % e
+        #    sys.exit(1)
 
-        print 'OK!\n'
-        print 'Firmware:', self.firmware
-        print
+        #print 'OK!\n'
+        #print 'Firmware:', self.firmware
+        #print
 
     @MonitorCommand
     def rr(self, args):
@@ -323,6 +343,132 @@ class InteractiveMonitor(Monitor):
 
         r0 = Monitor.call(self, addr, r0, r1, r2, r3)
         print '  Returned %08x' % r0
+
+    @MonitorCommand
+    def nvdsinfo(self, args):
+        data = ''.join(map(chr, self.read_mem(0x000006a4, 36)))
+        (write, erase, initialized, current_page,
+                tag_head, blob_head, free_space, page0, page1,
+                page_size) = struct.unpack('<IIBBxxIIIIII', data)
+        print '  Write proc = %08x' % write
+        print '  Erase proc = %08x' % erase
+        print '  Initialized = %d' % initialized
+        print '  Current page = %d' % current_page
+        print '  Tag head = %08x' % tag_head
+        print '  Blob head = %08x' % blob_head
+        print '  Free space = %d' % free_space
+        print '  Page 0 = %08x' % page0
+        print '  Page 1 = %08x' % page1
+        print '  Page size = %d' % page_size
+
+    @MonitorCommand
+    def nvdsread(self, args):
+        (tag, size) = (0, 0)
+        try:
+            if len(args) > 0:
+                tag = int(args[0], 0)
+            if len(args) > 1:
+                size = int(args[1], 0)
+        except:
+            print 'Invalid arguments'
+            return
+
+        r0 = Monitor.call(self, 0x010119f5, tag, size, 0x00011000)
+        if r0 == 1:
+            print '  File found!'
+            data = self.read_mem(0x00011000, size)
+            print '  ',
+            print ' '.join(map(lambda x: '%02x' % x, data))
+        else:
+            print '  File not found'
+
+    @MonitorCommand
+    def load(self, args):
+        if len(args) < 1:
+            print 'Need a filename';
+            return
+        try:
+            f = file(args[0])
+            img = f.read()
+            f.close()
+            (magic, flags, text, etext, data, bss, ebss, chksum) = struct.unpack('<IIIIIIII', img[0:32])
+            if magic != 0x414d5249:
+                raise Exception()
+        except:
+            print 'Unable to read file'
+            return
+        imgsize = len(img)
+        print '  Image size: %d bytes' % imgsize
+
+        if Monitor.plugin(self, 0) > 0:
+            print '  Plugin is running, stopping...'
+            Monitor.plugin(self, 2)
+
+        print '  Erasing flash...'
+        addr = 0x01060000
+        while addr < 0x01060000+imgsize:
+            print '  %08x' % addr
+            self.erase_flash(addr, 1)
+            addr += 0x10000
+
+        print '  Writing to flash...'
+        addr = 0x01060000
+        while len(img) > 0:
+            d = img[0:16]
+            print '  %08x' % addr
+            self.write_flash(addr, d)
+            addr = addr + len(d)
+            img = img[16:]
+
+        print '  Starting plugin...',
+        if Monitor.plugin(self, 1) > 0:
+            print 'okay!'
+        else:
+            print 'failed.'
+
+    @MonitorCommand
+    def dump(self, args):
+        (addr, length, filename) = (0, 32, 'dump.bin')
+        try:
+            if len(args) > 0:
+                addr = int(args[0], 0)
+            if len(args) > 1:
+                length = int(args[1], 0)
+            if len(args) > 2:
+                filename = args[2]
+        except:
+            print 'Invalid arguments'
+            return
+        try:
+            f = file(filename, 'w')
+        except:
+            print 'Unable to open file %s for writing' % filename
+            return
+
+        data = self.read_mem(addr, length)
+        f.write(''.join(map(chr, data)))
+        f.close()
+
+        print 'Dumped %d bytes to %s' % (len(data), filename)
+
+    @MonitorCommand
+    def plugin(self, args):
+        if len(args) < 1 or args[0] == 'status':
+            if Monitor.plugin(self, 0) > 0:
+                print 'Plugin is running'
+            else:
+                print 'Plugin is not running'
+        elif args[0] == 'start':
+            print 'Starting plugin...',
+            if Monitor.plugin(self, 1) > 0:
+                print 'okay!'
+            else:
+                print 'failed'
+        elif args[0] == 'stop':
+            print 'Stopping plugin'
+            Monitor.plugin(self, 2)
+        else:
+            print 'Unknown action %s' % args[0]
 
     @MonitorCommand
     def quit(self, args):
@@ -380,5 +526,9 @@ class InteractiveMonitor(Monitor):
             self.invoke(l)
 
 if __name__ == '__main__':
-    InteractiveMonitor().run('00:80:37:14:42:df')
+    if len(sys.argv) > 1:
+        addr = sys.argv[1]
+    else:
+        addr = '00:80:37:14:42:df'
+    InteractiveMonitor().run(addr)
 
