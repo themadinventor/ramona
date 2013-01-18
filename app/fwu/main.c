45f0a1f51425912146f25bd6a53bd6aa5fccc616
Fredrik Ahlberg <fredrik@z80.se>
2013-01-18 03:47:01 +0100

    ota fwu working!

--- app/fwu/main.c
+++ app/fwu/main.c
@@ -24,42 +24,34 @@ struct fwheader {
 
 void start(void)
 {
+    *((unsigned char *) 0x00800110) |= 0x02;
+
     UARTInit();
     UART2SetBaudRate(UART_115200);
     UART2WriteString("IRMA FWU\n\r");
-    //goto leave;
 
     void *payload_base = &_etext + (&_edata - &_data);
-    /*UART2WriteString("Payload base at ");
-    WriteHex((unsigned int) payload_base);
-    UART2WriteString("\n\r");*/
 
     struct fwheader *header = payload_base;
-    //UART2WriteString("Magic: "); WriteHex(header->magic);
-    /*UART2WriteString("\n\rChecksum: "); WriteHex(header->checksum);
-    UART2WriteString("\n\rSize: "); WriteHex(header->size);
-    UART2WriteString("\n\r");*/
 
     if (header->magic != OS_MAGIC) {
         UART2WriteString("Fatal: Wrong magic\n\r");
-        goto leave;
+        goto boot;
     }
    
     if (header->size > OS_MAX_SIZE) {
         UART2WriteString("Fatal: Firmware is too large\n\r");
-        goto leave;
+        goto boot;
     }
 
-    UART2WriteString("Calculating checksum...\n\r");
+    //UART2WriteString("Calculating checksum...\n\r");
     unsigned short checksum = ROM_CRC16(payload_base+8, header->size-8, 0);
     if (checksum != header->checksum) {
         UART2WriteString("Fatal: Invalid checksum\n\r");
-        goto leave;
+        goto boot;
     }
 
-    //goto leave;
-
-    UART2WriteString("Flash stuff...\n\r");
+    //UART2WriteString("Flash stuff...\n\r");
 
     /* Apply voodoo to unlock flash */
     *((unsigned char *) 0x00800308) = 0x1e;
@@ -71,50 +63,60 @@ void start(void)
         UART2WriteString("Fatal: Unknown flash (");
         WriteHex(flash);
         UART2WriteString(")\n\r");
-        goto leave;
+        goto boot;
     }
 
-    UART2WriteString("Erasing flash...\n\r");
+    //UART2WriteString("Erasing flash...\n\r");
     //for (unsigned int addr = FLASH_FIRST_BASE; addr < FLASH_LIMIT; addr += FLASH_PAGE_SIZE) {
     for (unsigned int addr = FLASH_BASE; addr < FLASH_LIMIT; addr += FLASH_PAGE_SIZE) {
-        WriteHex(addr);
+        //WriteHex(addr);
         
         if (flash_erase(addr)) {
-            UART2WriteString(" failed\n\rFatal: Failed to erase page\n\r");
-            goto failed;
-        } else {
-            UART2WriteString(" ok\n\r");
+            UART2WriteString("Fatal: Failed to erase page\n\r");
+            goto reboot;
+        //} else {
+            //UART2WriteString(" ok\n\r");
         }
+
+        *((unsigned char *) 0x00800110) ^= 0x02;
     }
+    //UART2WriteString("Flash empty\n\r");
+    //goto leave;
 
-    unsigned int imgsize = 0; 
-
-    UART2WriteString("Writing to flash...");
+    //UART2WriteString("Writing to flash...");
     //for (unsigned int addr = FLASH_FIRST_BASE; addr < FLASH_FIRST_BASE + imgsize; addr += 2) {
     unsigned short *src = payload_base;
-    for (unsigned int addr = FLASH_BASE; addr < FLASH_BASE + imgsize; addr += 2) {
+    for (unsigned int addr = FLASH_BASE; addr < FLASH_BASE + header->size; addr += 2) {
         flash_write(addr, *src++); 
 
-        if (addr & 0xffffff00) {
-            WriteHex(addr);
-            UART2WriteString("\n\r");
+        if (!(addr & 0x00000fff)) {
+            //WriteHex(addr);
+            //UART2WriteString("\n\r");
+            *((unsigned char *) 0x00800110) ^= 0x02;
         }
     }
 
-    UART2WriteString("\n\rDone!\n\r");
+    // Destroy our signature so we won't run again
+    flash_write(FLASH_LIMIT, 0); 
 
-leave:
-    /* Flush UART */
-    //while (UART2GetTxFIFOSize() < 127) ;
-    for (int i=0; i<0xfffff; i++) ;
-    return;
+reboot:
+    //UART2WriteString("\n\rWill reboot.\n\r");
 
-failed:
-    UART2WriteString("\n\rWill reboot.\n\r");
+    *((unsigned char *) 0x00800110) |= 0x02;
 
-    *((unsigned int *)0x00800c0c) = 0xc0;
-    *((unsigned int *)0x00800c0c) = 0x1c;
+    *((unsigned int *) 0x00800c0c) = 0xc0;
+    *((unsigned int *) 0x00800c0c) = 0x1f;
+
+    *((unsigned char *) 0x00800910) = 0x00;
+    *((unsigned char *) 0x00800914) = 0x04;
+    *((unsigned char *) 0x00800910) = 0x01;
 
     for (;;) ;
+
+boot:
+    /* Flush UART */
+    //while (UART2GetTxFIFOSize() < 127) ;
+    //for (int i=0; i<0xfffff; i++) ;
+    return;
 }
 
